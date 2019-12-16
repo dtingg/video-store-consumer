a06dfbd6393e9d9c102032f3a7359aa9bb397ce8
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-12-16 14:27:59 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,87 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedMovie: 'Happy Feet',
+      selectedCustomer: 'Penguins',
+    };
   }
+
+  render() {
+// export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <p>{this.state.selectedCustomer}</p>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home movie={this.state.selectedMovie}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = (props) => {
+
+// function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>{props.movie}</p>
+    </div>
+  )
+}
+
+export default Home;
