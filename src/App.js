21c812a96a2afec72a37d7b602d26e6c92330c72
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-12-17 09:16:16 -0800

    added formatting and proptype for customers

--- src/App.js
+++ src/App.js
@@ -15,22 +15,6 @@ import Search from './components/Search';
 import Library from './components/Library';
 import Customers from './components/Customers';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
 class App extends Component {
   constructor(props) {
     super(props);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './Customer.css';
 
-
-
-
 const Customer = (props) => {
   const { id, name, registered_at, address, city, state, postal_code, phone, account_credit, movies_checked_out_count } = props;
 
@@ -20,23 +17,33 @@ const Customer = (props) => {
 
   return (
     <div>
-      {id}
-      {name}
-      {registered_at}
-      {address}
+      <p>{ id }</p>
+      <p>{ name }</p>
+      <p>{ registered_at }</p>
+      <p>{ address }</p>
+      <p>{ city }</p>
+      <p>{ state }</p>
+      <p>{ postal_code }</p>
+      <p>{ phone }</p>
+      <p>{ account_credit }</p>
+      <p>{ movies_checked_out_count }</p>
     </div>
   );
 };
 
-// PetCard.propTypes = {
-//   id: PropTypes.number.isRequired,
-//   name: PropTypes.string.isRequired,
-  // species: PropTypes.string.isRequired,
-  // location: PropTypes.string,
-  // images: PropTypes.array,
-  // about: PropTypes.string,
-  // selectPet: PropTypes.func.isRequired,
+Customer.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  registered_at: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  postal_code: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  account_credit: PropTypes.number.isRequired,
+  movies_checked_out_count: PropTypes.number.isRequired,
+  // postal_code: PropTypes.func.isRequired,
   // removePet: PropTypes.func,
-// }
+}
 
 export default Customer;
