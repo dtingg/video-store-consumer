6b3e90f9440eb6a9600d66fa4832b541d1d4cc22
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-12-16 16:25:14 -0800

    updated search branch

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,10 +11,62 @@ class Search extends Component {
     };
   }
 
+  onFieldChange = (event) => {
+    const updatedState = {};
+
+    const name = event.target.name;
+    const value = event.target.value;
+
+    updatedState[name] = value;
+    this.setState(updatedState);
+  }
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (this.state.query) {
+      // this.props.addCardCallback(this.state);
+
+      const search_url = `${this.props.baseUrl}/movies`;
+
+      axios.get(customer_url)
+        .then((response) => {
+          this.setState({ customers: response.data });
+        })
+        .catch((error) => {
+          this.setState({ error: error.message });
+        });
+
+      this.setState({
+        query: '',
+      });
+    }
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Search</h2>
+        <form onSubmit={ this.onSubmitHandler }>
+          <h1>Search for a Movie</h1>
+          <div >
+            <div>
+              <label htmlFor="query">Query: </label>
+              <input
+                name="query"
+                id="query"
+                onChange={ this.onFieldChange }
+                value={ this.state.query }
+              />
+            </div>
+          </div>
+          <input
+            type="submit"
+            name="submit"
+            value="Search"
+            onClick={ this.onSubmitHandler }
+          />
+        </form>
       </div>
     )
   }
