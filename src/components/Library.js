580a28060719d188eff5606ce56b09b4feefd604
Dianna Tingg <dtingg@gmail.com>
2019-12-19 13:07:11 -0800

    added page name and changed movie key to external_id

--- src/components/Library.js
+++ src/components/Library.js
@@ -6,7 +6,7 @@ import './Library.css';
 
 const Library = ({ library, movieCallback }) => {
   const rentalLibrary = library.map((movie) => {
-    return <Movie key={movie.id}
+    return <Movie key={movie.external_id}
     {...movie}
     movieCallback={movieCallback}
     action={"Select Movie"} />
