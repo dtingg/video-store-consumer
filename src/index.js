da7d2294e1da6ebd2a72bae9173b0b606ec41d27
Dianna Tingg <dtingg@gmail.com>
2019-12-18 19:57:34 -0800

    changed url back to https

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App baseUrl="wss://bkr-video-api.herokuapp.com"/>, document.getElementById('root'));
+ReactDOM.render(<App baseUrl="https://bkr-video-api.herokuapp.com"/>, document.getElementById('root'));
 registerServiceWorker();
